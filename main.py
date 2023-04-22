08c4961c2f4ad471d6a0c67858535462b2802a6d
Girbo2312 <nosokosospidaras@gmail.com>
2023-04-22 10:54:37 +0300

    add pygame base

---
+++ main.py
@@ -0,0 +1,38 @@
+from pygame import*
+
+fps=60
+
+back = (200, 255, 255)
+
+width = 600
+height = 500
+window = display.setmode((width,height))
+window.fill(back)
+
+clock = time.Clock()
+
+game = True
+finish = False
+
+"""
+....
+...
+....
+"""
+
+while game:
+
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+
+    ...
+    display.update()
+    clock.tick()
+
+
+
+
+
+
